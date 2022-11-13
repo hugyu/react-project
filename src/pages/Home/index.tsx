16db296209aa7e43e6882299e11f64048af211e2
hgy <1500586026@qq.com>
2022-11-13 14:49:17 +0800

    前后端接口的接通

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,22 +4,44 @@ import { Navigate } from "react-router-dom";
 import axios from "axios";
 import "./style.css";
 const Home: React.FC = () => {
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
+  const handleLogoutClick = () => {
+    axios.get("/api/logout").then((res) => {
+      if (res.data?.data) {
+        setIsLogin(false);
+        console.log("退出成功");
+      } else {
+        console.log("退出失败");
+      }
+    });
+    };
+    const handleCrawllerClick = () => {
+        axios.get("/api/getData").then((res) => {
+            if (res.data?.data) {
+              
+            } else {
+              console.log("退出失败");
+            }
+          });
+    }
   useEffect(() => {
     axios.get("/api/isLogin").then((res) => {
       if (res.data?.data) {
         setIsLogin(true);
         console.log("已经登录");
       } else {
+        setIsLogin(false);
         console.log("未登录");
       }
     });
   }, []);
   return isLogin ? (
     <div className="home-page">
-      <Button type="primary">爬取内容</Button>
+      <Button type="primary" onClick={handleCrawllerClick}>爬取内容</Button>
       <Button type="primary">展示数据</Button>
-      <Button type="primary">退出</Button>
+      <Button type="primary" onClick={handleLogoutClick}>
+        退出
+      </Button>
     </div>
   ) : (
     <Navigate to={"/login"} />
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,14 +1,60 @@
 import { LockOutlined } from "@ant-design/icons";
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, message } from "antd";
+import axios from "axios";
+import qs from "qs";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 import "./style.css";
 interface FormValues {
   password: string;
 }
-const onFinish = (values: FormValues) => {
-  console.log("Received values of form: ", values);
-};
+
 function LoginPage() {
-  return (
+  const [isLogin, setIsLogin] = useState(false);
+  const onFinish = (values: FormValues) => {
+    axios
+      .post("/api/login", qs.stringify({ password: values.password }), {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      })
+      .then((res) => {
+        if (res.data?.data) {
+          setIsLogin(true);
+        } else {
+          message.error("登录失败");
+        }
+      });
+    };
+    // return (
+    //     <div className="login-page">
+    //   <Form
+    //     name="normal_login"
+    //     className="login-form"
+    //     initialValues={{ remember: true }}
+    //     onFinish={onFinish}
+    //   >
+    //     <Form.Item
+    //       name="password"
+    //       rules={[{ required: true, message: "Please input your Password!" }]}
+    //     >
+    //       <Input
+    //         prefix={<LockOutlined className="site-form-item-icon" />}
+    //         type="password"
+    //         placeholder="Password"
+    //       />
+    //     </Form.Item>
+    //     <Form.Item>
+    //       <Button
+    //         type="primary"
+    //         htmlType="submit"
+    //         className="login-form-button"
+    //       >
+    //         Log in
+    //       </Button>
+    //     </Form.Item>
+    //   </Form>
+    // </div>
+    // )
+  return !isLogin ? (
     <div className="login-page">
       <Form
         name="normal_login"
@@ -37,6 +83,8 @@ function LoginPage() {
         </Form.Item>
       </Form>
     </div>
+  ) : (
+    <Navigate to={"/"} />
   );
 }
 export default LoginPage;
