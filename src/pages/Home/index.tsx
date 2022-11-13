9ce4877b678718fe8c7dc4ba17cb720ff678f27d
hgy <1500586026@qq.com>
2022-11-13 15:06:53 +0800

    安装echarts并展示出图表

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "antd";
 import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import ReactEcharts from "echarts-for-react";
 import axios from "axios";
 import "./style.css";
 const Home: React.FC = () => {
@@ -14,16 +15,79 @@ const Home: React.FC = () => {
         console.log("退出失败");
       }
     });
+  };
+  const handleCrawllerClick = () => {
+    axios.get("/api/getData").then((res) => {
+      if (res.data?.data) {
+      } else {
+        console.log("退出失败");
+      }
+    });
+  };
+  const getOption: () => echarts.EChartsOption = () => {
+    return {
+      title: {
+        text: "Stacked Line",
+      },
+      tooltip: {
+        trigger: "axis",
+      },
+      legend: {
+        data: ["Email", "Union Ads", "Video Ads", "Direct", "Search Engine"],
+      },
+      grid: {
+        left: "3%",
+        right: "4%",
+        bottom: "3%",
+        containLabel: true,
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {},
+        },
+      },
+      xAxis: {
+        type: "category",
+        boundaryGap: false,
+        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+      },
+      yAxis: {
+        type: "value",
+      },
+      series: [
+        {
+          name: "Email",
+          type: "line",
+          stack: "Total",
+          data: [120, 132, 101, 134, 90, 230, 210],
+        },
+        {
+          name: "Union Ads",
+          type: "line",
+          stack: "Total",
+          data: [220, 182, 191, 234, 290, 330, 310],
+        },
+        {
+          name: "Video Ads",
+          type: "line",
+          stack: "Total",
+          data: [150, 232, 201, 154, 190, 330, 410],
+        },
+        {
+          name: "Direct",
+          type: "line",
+          stack: "Total",
+          data: [320, 332, 301, 334, 390, 330, 320],
+        },
+        {
+          name: "Search Engine",
+          type: "line",
+          stack: "Total",
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+        },
+      ],
     };
-    const handleCrawllerClick = () => {
-        axios.get("/api/getData").then((res) => {
-            if (res.data?.data) {
-              
-            } else {
-              console.log("退出失败");
-            }
-          });
-    }
+  };
   useEffect(() => {
     axios.get("/api/isLogin").then((res) => {
       if (res.data?.data) {
@@ -37,11 +101,15 @@ const Home: React.FC = () => {
   }, []);
   return isLogin ? (
     <div className="home-page">
-      <Button type="primary" onClick={handleCrawllerClick}>爬取内容</Button>
-      <Button type="primary">展示数据</Button>
-      <Button type="primary" onClick={handleLogoutClick}>
-        退出
-      </Button>
+      <div className="buttons">
+        <Button type="primary" onClick={handleCrawllerClick}>
+          爬取
+        </Button>
+        <Button type="primary" onClick={handleLogoutClick}>
+          退出
+        </Button>
+      </div>
+      <ReactEcharts option={getOption()} />
     </div>
   ) : (
     <Navigate to={"/login"} />
