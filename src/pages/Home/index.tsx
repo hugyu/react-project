54145b18afe4bf2da0bcf593aa256878579c5252
hgy <1500586026@qq.com>
2022-11-13 13:54:39 +0800

    未登录内容重定向

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,28 @@
 import { Button } from "antd";
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import axios from "axios";
 import "./style.css";
 const Home: React.FC = () => {
-  return (
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    axios.get("/api/isLogin").then((res) => {
+      if (res.data?.data) {
+        setIsLogin(true);
+        console.log("已经登录");
+      } else {
+        console.log("未登录");
+      }
+    });
+  }, []);
+  return isLogin ? (
     <div className="home-page">
       <Button type="primary">爬取内容</Button>
       <Button type="primary">展示数据</Button>
       <Button type="primary">退出</Button>
     </div>
+  ) : (
+    <Navigate to={"/login"} />
   );
 };
 export default Home;
