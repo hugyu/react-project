ac2928b0c19bd1de9e641a0de7617b6b1ff81f2b
hgy <1500586026@qq.com>
2022-11-13 11:45:14 +0800

    编写登录页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input } from 'antd';
+import React from 'react';
+import './login.css'
+const App: React.FC = () => {
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values);
+  };
 
-function App() {
   return (
-    <div className="App">
-      <div>hello world</div>
-    </div>
+    <div className='login-page'>
+       <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+    >
+ 
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Please input your Password!' }]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+      {/* <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <a className="login-form-forgot" href="">
+          Forgot password
+        </a>
+      </Form.Item> */}
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+        {/* Or <a href="">register now!</a> */}
+      </Form.Item>
+    </Form>
+   </div>
   );
-}
+};
 
 export default App;
